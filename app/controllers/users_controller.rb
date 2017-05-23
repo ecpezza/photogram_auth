e4e7b4d1593650f138b10228b8a448d987ce199d
Elizabeth C. Pezza <ecpezza@users.noreply.github.com>
2017-05-23 09:20:51 -0500

    Before class

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+
+    render("users/index.html.erb")
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    render("users/show.html.erb")
+  end
+
+  def like
+    @user = current_user
+
+    render("users/like.html.erb")
+
+  end
+
+end
